b439ddb8c4a77b367c1a8bb733509f8b43cf6925
fatikkkk <98143123+fatikkkk@users.noreply.github.com>
2022-10-08 15:26:59 +0500

    Add project files.

---
+++ Calculate/Calculation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculate
+{
+    public class Calculation
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Error");
+                return 0;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+        public int Percent(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
---
+++ CalculateProject/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Calculate;
+
+namespace CalculateProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите два числа:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы ввели следующие числа: " + a + " и " + b);
+            Console.WriteLine("Введите операцию");
+            string oper = Console.ReadLine();
+            Console.WriteLine("Вы выбрали следуюущую операцию: " + oper);
+            Calculation calculate = new Calculation();
+            int res = 0;
+            switch (oper)
+            {
+                case "+":
+                    {
+                        res = calculate.Add(a, b);
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    }
+                case "-":
+                    {
+                        res = calculate.Sub(a, b);
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    }
+                case "*":
+                    {
+                        res = calculate.Multiplication(a, b);
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    }
+                case "/":
+                    {
+                        res = calculate.Division(a, b);
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    }
+                case "%":
+                    {
+                        res = calculate.Percent(a, b);
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Выбрана неверная операция");
+                        break;
+                    }
+            }
+        }
+    }
+}
